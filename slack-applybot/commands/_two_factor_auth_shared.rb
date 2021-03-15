18a9cffbeaf0b4539baeecf8ef4618ac7066758a
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-03-15 07:16:46 +0000

    Rename the 2fa shared module

--- slack-applybot/commands/_two_factor_auth_shared.rb
+++ slack-applybot/commands/_two_factor_auth_shared.rb
@@ -1,7 +1,11 @@
-module TwoFactorAuth
+module TwoFactorAuthShared
   require 'rotp'
 
   private
+
+  def validate_otp_part(otp)
+    validate(user, otp) if otp.match(/\d*/)
+  end
 
   def validate(user, otp)
     totp = ROTP::TOTP.new(Encryption::Service.decrypt(user.encrypted_2fa_secret), issuer: ENV.fetch('SERVICE_NAME'))
